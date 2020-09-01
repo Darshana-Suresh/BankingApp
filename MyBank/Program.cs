78e8b6588905c3ae50e69dcdbe037d5145a78622
darshana suresh <dashysuresh@gmail.com>
2020-09-01 15:01:41 +0530

    project files

---
+++ MyBank/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace BankLib
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var account = new BankAccount("Dashy", 10000);
+            Console.WriteLine($"Account {account.Number} was created for {account.Owner} with ${account.Balance}.");
+            account.MakeWithdrawal(120, DateTime.Now, "Hammock");
+            account.MakeWithdrawal(4, DateTime.Now, "a");
+            account.MakeWithdrawal(10, DateTime.Now, "s");
+            account.MakeWithdrawal(220, DateTime.Now, "d");
+
+            Console.WriteLine($"Balance after withdrawal of $120 is ${account.Balance})");
+
+            Console.WriteLine(account.GetAccountHistory());
+
+        }
+    }
+}
